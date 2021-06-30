a95d221e03a1c7b4f9f1c8176398153af971d1a5
morganjames1 <ronaldusmorganjames@gmail.com>
2021-06-30 09:21:02 +0700

    Add javascript

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const animals = [
+    { name: "Fluffy", species: "cat", class: { name: "mamalia" } },
+    { name: "Carlo", species: "dog", class: { name: "vertebrata" } },
+    { name: "Nemo", species: "fish", class: { name: "mamalia" } },
+    { name: "Hamilton", species: "dog", class: { name: "mamalia" } },
+    { name: "Dory", species: "fish", class: { name: "mamalia" } },
+    { name: "Ursa", species: "cat", class: { name: "mamalia" } },
+    { name: "Taro", species: "cat", class: { name: "vertebrata" } }
+];
+
+for (let i = 0; i < animals.length; i++) {
+    if (animals[i].species == 'cat') {
+        console.log(animals[i]);
+    }
+}
+
+
+console.log("#--------#")
+
+var find = animals.filter(function (cari) {
+    return cari.species == "cat";
+});
+
+console.log(find);
+
+
+
+console.log("#-----#");
+
+for (let i = 0; i < animals.length; i++) {
+    if (animals[i].class.name == 'mamalia') {
+        animals[i].class.name = "Mamalia";
+        console.log(animals[i]);
+    } else {
+        animals[i].class.name = "Non - mamalia";
+        console.log(animals[i]);
+    }
+}
+
+
+
